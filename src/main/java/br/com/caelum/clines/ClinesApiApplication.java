649f050e82d3069eca44e3597979413c55907d51
Maite <maicapref@gmail.com>
2023-02-01 14:31:50 -0300

    teste 894889848526526

--- src/main/java/br/com/caelum/clines/ClinesApiApplication.java
+++ src/main/java/br/com/caelum/clines/ClinesApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile({"dev", "prod"})
 public class ClinesApiApplication {
 
     public static void main(String[] args) {
