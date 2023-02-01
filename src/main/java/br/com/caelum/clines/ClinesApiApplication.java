908a82a54e8e5ec8b66281fcee5017a0c77b75b4
Maite <maicapref@gmail.com>
2023-02-01 14:03:42 -0300

    testando deploy vez 394390482349234

---
+++ src/main/java/br/com/caelum/clines/ClinesApiApplication.java
@@ -0,0 +1,15 @@
+package br.com.caelum.clines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class ClinesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinesApiApplication.class, args);
+    }
+
+}
